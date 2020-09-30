669a276202ab977462c3dcc1a0c6083da4a84542
Dennis Mimi <dennismimi66@gmail.com>
2020-09-30 09:58:36 +0300

    version 2

--- Mutations/views.py
+++ Mutations/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render,HttpResponse
 
 def index(request):
-    return render(request,'index.html',{})
+    return render(request,'login.html',{})
+
+def register(request):
+    return render(request,'register.html',{})
